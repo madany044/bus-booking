420bb8bca5ed3205134c6018ea830e539df92f3e
madany044 <madanmadany2004@gmail.com>
2026-03-23 21:13:02 +0530

    modified footer details

--- frontend/src/components/common/Footer.tsx
+++ frontend/src/components/common/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="container footer-inner">
-        <span>© 2025 BusSearch. All rights reserved.</span>
+        <span>© 2026 BusSearch. All rights reserved.</span>
         <div className="footer-links">
           <a href="#">Privacy Policy</a>
           <a href="#">Terms of Service</a>
